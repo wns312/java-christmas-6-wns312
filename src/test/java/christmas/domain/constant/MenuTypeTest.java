90e1aa97fef8da9cc0347d2ab953465200e7240d
wns312 <wns312@gmail.com>
2023-11-15 16:13:14 +0900

    test(MenuType): 테스트 케이스 작성

---
+++ src/test/java/christmas/domain/constant/MenuTypeTest.java
@@ -0,0 +1,20 @@
+package christmas.domain.constant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class MenuTypeTest {
+
+    @DisplayName("메뉴 이름 조회 테스트")
+    @ParameterizedTest
+    @EnumSource(MenuType.class)
+    void getMenuNameTest(MenuType menuType) {
+        String name = menuType.getName();
+
+        assertThat(MenuType.getByMenuName(name))
+                .isEqualTo(menuType);
+    }
+}
