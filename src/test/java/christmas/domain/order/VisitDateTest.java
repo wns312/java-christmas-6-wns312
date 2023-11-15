fecf02133dc99e487d3eda00d89186eff0e49707
wns312 <wns312@gmail.com>
2023-11-15 15:49:11 +0900

    test(VisitDate): 테스트 케이스 추가

--- src/test/java/christmas/domain/order/VisitDateTest.java
+++ src/test/java/christmas/domain/order/VisitDateTest.java
@@ -1,8 +1,8 @@
 package christmas.domain.order;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import christmas.domain.order.VisitDate;
 import christmas.exception.IllegalArgumentExceptionType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -25,5 +25,70 @@ class VisitDateTest {
         assertThatCode(() -> new VisitDate(date))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(IllegalArgumentExceptionType.INVALID_DATE.getMessage());
+    }
+
+    @DisplayName("일요일 아닌 날짜 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 30})
+    void checkIsSundayTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isSunday()).isFalse();
+    }
+
+    @DisplayName("일요일인 날짜 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 17, 24, 31})
+    void checkIsNotSundayTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isSunday()).isTrue();
+    }
+
+    @DisplayName("일요일인 날짜 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {25})
+    void checkIsChristmasTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isChristmas()).isTrue();
+    }
+
+    @DisplayName("일요일인 날짜 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 26, 27, 28, 29, 30, 31})
+    void checkIsNotChristmasTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isChristmas()).isFalse();
+    }
+
+    @DisplayName("Weekday 주중 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31})
+    void checkIsWeekDayTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isWeekDay()).isTrue();
+    }
+
+    @DisplayName("Weekday 주중 아님 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30})
+    void checkIsNotWeekDayTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isWeekDay()).isFalse();
+    }
+
+    @DisplayName("WeekEnd 주말 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30})
+    void checkIsWeekEndTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isWeekEnd()).isTrue();
+    }
+
+    @DisplayName("WeekEnd 주말 아님 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31})
+    void checkIsNotWeekEndTest(int date) {
+        VisitDate visitDate = new VisitDate(date);
+        assertThat(visitDate.isWeekEnd()).isFalse();
     }
 }
